04c9e1b44f92ad8cbd60c9535b5f098a833da5b5
edwardash <edwardash00@gmail.com>
2022-12-31 16:18:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import Navbar from './components/Navbar/Navbar';
+
+import { BrowserRouter, Switch, Route, Redirect, useLocation } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Auth from './components/AUth/Auth';
+import PostDetails from './components/PostDetails/PostDetails';
+import ChipInput from 'material-ui-chip-input';
+
+
+
+const App = () => {
+
+  const user = JSON.parse(localStorage.getItem('profile'));
+
+
+  return (
+    <BrowserRouter>
+      <Container maxWidth="xl">
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={() => <Redirect to='/posts' />} />
+
+          <Route path={'/posts'} exact component={Home} />
+          <Route path={'/posts/search'} exact component={Home} />
+          <Route path={'/posts/:id'} exact component={PostDetails} />
+
+          <Route path='/auth' exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
+
+
+  );
+}
+
+export default App;
